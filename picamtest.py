1e47fb1c9aef1f74d11ef19a68a97a49f9bf75fc
heartworm <shravanlal98@gmail.com>
2017-08-14 14:28:54 +1000

    test program for pi camera

---
+++ picamtest.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+from pi_stream import PiStream
+import test
+
+showVideo = True
+
+
+def show_val(event,x,y,flags,param):
+    if event != cv2.EVENT_LBUTTONDOWN:
+        return
+    pixel = hsv[y,x]
+    print("HSV at {}, {}: ({}, {}, {})".format(x, y, pixel[0], pixel[1], pixel[2]))
+
+
+cv2.namedWindow('videooutput')
+cv2.setMouseCallback('videooutput', show_val)
+
+cv2.namedWindow('ballfinder')
+
+with PiStream as stream:
+    while showVideo:
+        img = stream.get_frame()
+        img = test.denoise(img)
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+        ballMask = test.getBallMaskHSV(hsv)
+
+        cv2.imshow('ballfinder', ballMask)
+
+        keypoints = test.getBallBlobKeypoints(255 - ballMask)
+        img = cv2.drawKeypoints(img, keypoints, np.array([]), (255, 255, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+
+        cv2.imshow('videooutput', img)
+
+
+        if cv2.waitKey(1) != -1:
+            showVideo = False
+
+cv2.destroyAllWindows()
