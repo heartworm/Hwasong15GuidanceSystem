0a373f80bab7a6cf7eda034ade50820739f89ce0
heartworm <shravanlal98@gmail.com>
2017-08-14 15:48:49 +1000

    fps display

--- picamtest.py
+++ picamtest.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from pi_stream import PiStream
 import test
+import timeit
 
 showVideo = True
 
@@ -18,8 +19,23 @@ cv2.setMouseCallback('videooutput', show_val)
 
 cv2.namedWindow('ballfinder')
 
+fps_lasttime = timeit.default_timer()
+fps = 0
+alpha = 0.95
 
-with PiStream(resolution=(160,120)) as stream:
+def get_fps():
+    global fps, fps_lasttime
+    newtime = timeit.default_timer()
+    dtime = newtime - fps_lasttime
+    newfps = 1 / dtime
+    fps = newfps * (1 - alpha) + fps * alpha
+    fps_lasttime = newtime
+    return fps
+
+
+resolution = (160,120)
+
+with PiStream(resolution = resolution) as stream:
     while showVideo:
         img = stream.get_frame()
         denoised = test.denoise(img)
@@ -33,6 +49,9 @@ with PiStream(resolution=(160,120)) as stream:
 
         keypoints = test.getBallBlobKeypoints(255 - ballMask)
         img = cv2.drawKeypoints(img, keypoints, np.array([]), (255, 255, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        fps_str = str(round(get_fps(), 2))
+        cv2.putText(img, fps_str, (10, resolution[1] - 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255,0,0))
 
 
         cv2.imshow('videooutput', img)
