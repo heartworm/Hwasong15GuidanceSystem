f66d92fa4472184b051ffb37e6736e107530b823
Shravan Lal <shravanlal98@gmail.com>
2017-09-17 12:24:28 +1000

    restructure

--- cv.py
+++ cv.py
@@ -1,5 +1,5 @@
 #import required modules
-import vrep
+import VREP.vrep
 import pygame
 import cv2
 from omni import Omni
@@ -8,21 +8,21 @@ from vision import ImageAnalyser
 import time
 
 print ('Program started')
-vrep.simxFinish(-1) # just in case, close all opened connections
+VREP.vrep.simxFinish(-1) # just in case, close all opened connections
 
-clientID = vrep.simxStart('127.0.0.1',12345,True,True,5000,5) # Connect to V-REP
+clientID = VREP.vrep.simxStart('127.0.0.1', 12345, True, True, 5000, 5) # Connect to V-REP
 analyser = ImageAnalyser()
 
 if clientID != -1:
     print('connected')
-    camera = vrep.simxGetObjectHandle(clientID, "Camera1", vrep.simx_opmode_blocking)[1]
-    ball = vrep.simxGetObjectHandle(clientID, "ball", vrep.simx_opmode_blocking)[1]
+    camera = VREP.vrep.simxGetObjectHandle(clientID, "Camera1", VREP.vrep.simx_opmode_blocking)[1]
+    ball = VREP.vrep.simxGetObjectHandle(clientID, "ball", VREP.vrep.simx_opmode_blocking)[1]
 
-    while vrep.simxGetConnectionId(clientID) != -1:
+    while VREP.vrep.simxGetConnectionId(clientID) != -1:
 
-        code, res, img = vrep.simxGetVisionSensorImage(clientID, camera, 0, vrep.simx_opmode_streaming)
+        code, res, img = VREP.vrep.simxGetVisionSensorImage(clientID, camera, 0, VREP.vrep.simx_opmode_streaming)
 
-        if code == vrep.simx_return_ok:
+        if code == VREP.vrep.simx_return_ok:
             img = np.array(img, dtype='uint8')
             img = np.reshape(img, list(res) + [3])
             img = np.swapaxes(img, 0, 1)
@@ -30,8 +30,8 @@ if clientID != -1:
             analyser.analyse(img)
             if analyser.ballPos is not None:
                 print(analyser.ballPos)
-                code, actualPos = vrep.simxGetObjectPosition(clientID, ball, camera, vrep.simx_opmode_streaming)
-                if code == vrep.simx_return_ok:
+                code, actualPos = VREP.vrep.simxGetObjectPosition(clientID, ball, camera, VREP.vrep.simx_opmode_streaming)
+                if code == VREP.vrep.simx_return_ok:
                     actualPos = actualPos[1:3]
                     print("Actual:", actualPos)
                     error = np.round(np.subtract(analyser.ballPos, actualPos) * 1000)
